a55c9c32b785e39c6a268305995547c1e478fd6f
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-01-27 23:52:32 -0600

    config, models and schema added

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const colors = require('colors');
 const cors = require('cors');
@@ -10,7 +9,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-// Connect to database
 connectDB();
 
 app.use(cors());
